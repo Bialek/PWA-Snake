ad9278f264d4e7277ecfbf8435dff91b8bbc25d8
Rafał Białek <rafal.bialek@intive.com>
2022-11-18 16:21:58 +0100

    update pwa

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,48 +4,29 @@ const STATIC_ASSETS = [
   './assets/style/style.css.map',
   './assets/script/snake.js',
   './assets/saves/',
-  // './assets/config.xml',
 ];
 
-const APP_PREFIX = 'Snake';
-const VERSION = '1.3';
-const CACHE_NAME = `${APP_PREFIX}_${VERSION}`;
-
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        return request;
-      } else {
-        return fetch(e.request);
-      }
+self.addEventListener('install', installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(STATIC_ASSETS);
+self.addEventListener('fetch', fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
     })
   );
 });
 
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      const cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      cacheWhitelist.push(CACHE_NAME);
-
-      return Promise.all(
-        keyList.map(function (key, i) {
-          if (cacheWhitelist.indexOf(key) === -1) {
-            return caches.delete(keyList[i]);
-          }
-        })
-      );
-    })
-  );
-});
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then(res => console.log('service worker registered'))
+      .catch(err => console.log('service worker not registered', err));
+  });
+}

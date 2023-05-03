aeee8a3d4283bcc70e423a0cdab724845e4eb17b
Rafał Białek <rbialek@blackberry.com>
2023-05-03 19:30:55 +0200

    fix: offline page

--- serviceWorker.js
+++ serviceWorker.js
@@ -7,7 +7,7 @@ const STATIC_ASSETS = [
 ];
 
 const APP_PREFIX = 'Snake';
-const VERSION = '1.5';
+const VERSION = '1.6';
 const CACHE_NAME = `${APP_PREFIX}_${VERSION}`;
 
 self.addEventListener('install', event => {

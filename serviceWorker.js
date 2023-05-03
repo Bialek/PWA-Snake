0ddb9304d1afae391958e01d65362f6513c7a949
Rafał Białek <rbialek@blackberry.com>
2023-05-03 20:05:36 +0200

    fix: add github page url

--- serviceWorker.js
+++ serviceWorker.js
@@ -7,7 +7,7 @@ const STATIC_ASSETS = [
 ];
 
 const APP_PREFIX = 'Snake';
-const VERSION = '1.8';
+const VERSION = '1.9';
 const CACHE_NAME = `${APP_PREFIX}_${VERSION}`;
 
 self.addEventListener('install', event => {
